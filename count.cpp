6bb339f911f067c6fc2823e0da0e8a2711422e39
Mohit <a1885648@adelaide.edu.au>
2023-07-25 14:10:12 +0930

    all changes

--- count.cpp
+++ count.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+//function
+int count(int array[], int n){
+    //integer to keep track of even numbers
+    int evenCount = 0;
+    //for loop that runs through the array and determines if number is divisible by 2. if so evenCount is increased by 1.
+    for (int i = 0; i < n; i++){
+        if (array[i] %2 == 0){
+            evenCount++;
+        }
+    }
+    //returns count
+    return evenCount;
+}
--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 extern int count(int[], int);
 
 int main() {
+    // array
     int array[5] = {4,5,6,7,8};
+    //prints the anwser
     std::cout << "The number is: " << count(array, 5) << std::endl;
     return 0;
 }
