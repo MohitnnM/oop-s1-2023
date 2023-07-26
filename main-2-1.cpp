6ab91621e7610b1c41eb0e3df50d98fe5e794e8c
Mohit <a1885648@adelaide.edu.au>
2023-07-26 11:34:24 +0930

    prac

---
+++ main-2-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// The `extern` keyword tells the compiler that the count function exists,
+// but will be implemented somewhere else
+extern int min_element(int[], int);
+
+int main(){
+     int array[5] = {10,5,2,7,8};
+     std::cout << "The minimum number is: " << min_element(array, 5) << std::endl;
+}
