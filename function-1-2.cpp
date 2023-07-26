9f6d4a948bffeb912a55e7485d44870efd2ca5fd
MohitnnM <140058914+MohitnnM@users.noreply.github.com>
2023-07-26 12:57:17 +0930

    Revert "pracs"

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 
 int array_mean(int array[], int n) {
-    //setting variables
     int sum = 0;
     double mean = 0;
-    //for loop to calculate sum of all elements in the array
+
     for (int i = 0; i < n; i++){
         sum += array[i];
     }
-    //if statement to check if variable n is less than 1
     if (n < 1) {
-        mean = 0.0;
-    } else{
-        //else statement to calculate mean if n >1
-        mean = sum/n;
+        sum = 0;
     }
-    //return value of mean
+    mean = sum/n;
     return mean;
 }
