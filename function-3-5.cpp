858397a0b3884271a29ea6a7ffef8f40e7267572
Mohit <a1885648@adelaide.edu.au>
2023-07-28 22:18:29 +0930

    prac

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -5,6 +5,8 @@ double sum_even(double array[], int n) {
   // setting variables
   double sum = 0.0;
 
+    // runs through array and checks is element is in even position
+    // if so then add it to sum
   for (int i = 0; i < n; i++) {
     if (i % 2 == 0){
         sum += array[i];
