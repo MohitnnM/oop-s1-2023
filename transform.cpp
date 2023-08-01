cdd23fb2f1b4b9c58368f5ed7e4277ee6c4fafcf
Mohit <a1885648@adelaide.edu.au>
2023-08-01 14:11:29 +0930

    workshop

---
+++ transform.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+void transform(int N, int base){
+    int quotient = N;
+    int remainder[10] = {};
+    int count = 0;
+
+    while (quotient !=0){
+        int division = quotient / base;
+        if (quotient % base != 0){
+            remainder[count] = 1;
+        } else if (quotient % base == 0) {
+            remainder[count] = 0;
+        }
+        quotient = floor(division);
+        count++;
+    }
+
+    for (int i = count - 1; i >= 0; i--){
+        std::cout<< remainder[i];
+    }
+    std::cout << std::endl;
+}
+int main(){
+    int N = 0;
+    int base = 0;
+    std::cout << "Enter integer to be converted: ";
+    std::cin >> N;
+    std::cout << "Enter base to convert to: ";
+    std::cin >> base;
+    transform(N,base);
+    return 0;
+}
