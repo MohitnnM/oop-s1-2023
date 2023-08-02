bbe4bfe2402e02c26212dd2aacc02431170035d4
Mohit <a1885648@adelaide.edu.au>
2023-08-02 09:58:02 +0930

    prac2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -3,8 +3,8 @@
 int is_identity(int array[10][10]) {
 
 
-    for (int i = 0; i < sizeof(array,1); i++){
-        for (int j = 0; j < sizeof(array,2); j++){
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 10; j++){
             if (i == j && array[i][j] != 1){
                 return 0;
             }
