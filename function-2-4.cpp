9b8d68a6399fde15f6407f774da82cd62d06c856
Mohit <a1885648@adelaide.edu.au>
2023-08-04 11:54:05 +0930

    prac2

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -6,10 +6,14 @@ int array_min(int integers[10], int length) {
   int min = integers[0];
   // loop through array and check if any element is  smaller then variable min
   // if so then min is equal to that element
-  for (int i = 0; i < length; i++) {
-    if (min > integers[i]) {
-      min = integers[i];
+  if (length > 0) {
+    for (int i = 0; i < length; i++) {
+      if (min > integers[i]) {
+        min = integers[i];
+      }
     }
+  } else {
+    return (-1);
   }
 
   return (min);
@@ -21,10 +25,14 @@ int array_max(int integers[10], int length) {
 
   // loop through array and check if any element is greater then variable max
   // if so then max is equal to that element
-  for (int i = 0; i < length; i++) {
-    if (max < integers[i]) {
-      max = integers[i];
+  if (length > 0) {
+    for (int i = 0; i < length; i++) {
+      if (max < integers[i]) {
+        max = integers[i];
+      }
     }
+  } else {
+    return (-1);
   }
   return (max);
 }
@@ -34,9 +42,13 @@ int sum_min_max(int integers[10], int length) {
   int sum = 0;
   int min = array_min(integers, length);
   int max = array_max(integers, length);
-  
+
   // sum min and max
-  sum = min + max;
+  if (length > 0) {
+    sum = min + max;
+  } else {
+    return (-1);
+  }
 
   return (sum);
 }
