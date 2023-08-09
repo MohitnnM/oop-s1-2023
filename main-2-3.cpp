08a4f04690e6a4cc10e4b64d34c4237a49931759
Mohit <a1885648@adelaide.edu.au>
2023-08-09 15:06:21 +0930

    k

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -4,8 +4,8 @@ using namespace std;
 extern int palindrome_sum(int[],int);
 
 int main(){
-    int integers[5] = {1,2,3,3,1};
-    int length = 5;
+    int integers[5] = {1,2,3,1};
+    int length = 4;
 
     cout << palindrome_sum(integers, length);
 
