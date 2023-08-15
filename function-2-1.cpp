4abd3578a4e77c3fd1e918a33ee2f2b258a3e839
Mohit <a1885648@adelaide.edu.au>
2023-08-15 22:46:50 +0930

    work4

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int* readNumbers(){
+    int* numbers = new int[10];
+    for (int i = 0; i < 10; i++){
+        cin >> numbers[i];
+    }
+
+    return numbers;
+}
+
 void hexDigits(int *numbers,int length){
     for (int i = 0; i < length; i++){
         switch (numbers[i])
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
+extern int* readNumbers();
 extern void hexDigits(int*, int);
 
 int main(){
     int length = 10;
-    int numbers[10] = {11,2,13,4,15,6,7,8,9,10};
+    int *numbers = readNumbers();
     hexDigits(numbers,length);
 }
