fde431190597d5adcf87ff62c0a2caa48e3c0bfe
Mohit <a1885648@adelaide.edu.au>
2023-08-18 11:24:52 +0930

    prac3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -2,6 +2,20 @@
 #include "Person.h"
 using namespace std;
 
+PersonList createPersonList(int n) {
+  Person* persons = new Person[n];
+  PersonList info;
+  info.numPeople = n;
+
+  for (int i = 0; i < info.numPeople; i++) {
+    persons[i].name = "Jane Doe";
+    persons[i].age = 1;
+  }
+  info.people = persons;
+
+  return info;
+}
+
 PersonList deepCopyPersonList(PersonList pl) {
   PersonList List = pl;
   Person* peoples = new Person[pl.numPeople];
