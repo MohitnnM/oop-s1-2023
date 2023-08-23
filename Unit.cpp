852f7b89a9cbceabb789e34926ab0e3023ba5c26
Mohit <a1885648@adelaide.edu.au>
2023-08-23 15:17:41 +0930

    e

--- Unit.cpp
+++ Unit.cpp
@@ -8,9 +8,7 @@ Unit::Unit(int u_unit_val, int u_num_beds, double u_unit_size){
    unit_size = u_unit_size;
 }
 
-int Unit::get_Num_Bedrooms(int num_beds){
-    return num_beds;
-}
+int Unit::get_Num_Bedrooms(int num_beds){return num_beds;}
 int Unit::get_Value(int unit_val){return unit_val;}
 double Unit::get_Area(double unit_size){return unit_size;}
 
--- Unit.h
+++ Unit.h
@@ -14,7 +14,7 @@ class Unit {
     unit_size = 0;
   }
   Unit(int unit_val, int num_beds, double unit_size);
-  int get_Num_Bedrooms(int num_beds){return num_beds;}
-  int get_Value(int unit_val){return unit_val;}
-  double get_Area(double unit_size){return unit_size;}
+  int get_Num_Bedrooms(int num_beds){};
+  int get_Value(int unit_val){};
+  double get_Area(double unit_size){};
 };
