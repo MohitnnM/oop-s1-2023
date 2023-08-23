261c433779c0234f2de31f1aac4a14ed9f929db7
Mohit <a1885648@adelaide.edu.au>
2023-08-23 14:25:08 +0930

    pract

--- test-1-3.cpp
+++ test-1-3.cpp
@@ -3,16 +3,15 @@ using namespace std;
 
 extern int* readNumbers();
 extern int* reverseArray(int*, int);
+extern bool equalsArray(int *, int*, int);
 
 int main(){
     int length = 5;
     int* numbers1 = readNumbers();
     int* reversedArray = reverseArray(numbers1,length);
 
+    cout << equalsArray(numbers1,reversedArray,length) << endl;
 
-
-    for (int i = 0; i < length; i++){
-        cout << reversedArray[i] << " ";
-    } 
-
+     delete[] numbers1;
+     delete[] reversedArray;
 }
--- testFunction-1-3.cpp
+++ testFunction-1-3.cpp
@@ -20,3 +20,22 @@ int* reverseArray(int* numbers1, int length) {
   }
   return reverseArray;
 }
+
+bool equalsArray(int *numbers1, int* numbers2, int length){
+    int count = 0;
+    if (length > 0){
+        for (int i = 0; i < length; i++){
+            if (numbers1[i] == numbers2[i]){
+                count++;
+            }
+        }
+        if (count == length){
+            return true;
+        } else {
+            return false;
+        }
+    }else{
+      return false;
+    }
+
+}
