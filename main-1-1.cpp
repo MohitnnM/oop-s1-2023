8f6f130d069ac954ad3754355e9076e49a693caa
Mohit <a1885648@adelaide.edu.au>
2023-08-23 15:06:41 +0930

    exam

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main(){
     Unit u1;
-    int area = u1.get_area(25);
-    int num_beds = u1.get_num_beds(4);
-    double price = u1.get_value(1000000);
+    int area = u1.get_Area(25);
+    int num_beds = u1.get_Num_Bedrooms(4);
+    double price = u1.get_Value(1000);
 
     cout << area << endl;
     cout << price << endl;
