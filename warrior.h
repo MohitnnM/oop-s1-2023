53d1185afacd155a8dae9163b9c0206fc1b7315d
Mohit <a1885648@adelaide.edu.au>
2023-08-29 19:12:58 +0930

    k

--- warrior.h
+++ warrior.h
@@ -1,8 +1,18 @@
 #ifndef WARRIOR_H
-#define WIZARD_H
+#define WARRIOR_H
 #include <iostream>
 #include "wizard.h"
+#include "player.h"
 using namespace std;
 
-
+class Warrior: public Player {
+    private:
+        string weapon;
+    public:
+    Warrior(string name, int health, int damage, string weapon);
+    string getName();
+    int getHealth();
+    string swingWeapon(Wizard*);
+    void castSpell(Player* opponent);    
+};
 #endif
--- wizard.h
+++ wizard.h
@@ -1,1 +1,17 @@
+#ifndef WIZARD_H
+#define WIZARD_H
 #include <iostream>
+#include "warrior.h"
+#include "player.h"
+using namespace std;
+
+class Wizard: public Player {
+    private:
+        int mana;
+    public:
+    Wizard(string name, int health, int damage, int mana);
+    string getName();
+    int getHealth();
+    void castSpell(Player* opponent);    
+};
+#endif
