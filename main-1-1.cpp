eca4287d0fc6f21c69087d9cfe47eda724499571
Mohit <a1885648@adelaide.edu.au>
2023-09-01 12:15:31 +0930

    k

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -5,17 +5,4 @@
 #include "Vehicle.h"
 using namespace std;
 
-int main() {
-    Vehicle* vehicleArray = new Vehicle[3];
-    Car c(1);
-    Bus b(2);
-    Motorbike m(3);
-
-    vehicleArray[0] = c;
-    vehicleArray[1] = b;
-    vehicleArray[2] = m;
-
-    for (int i = 0; i < 3; i++){
-        
-    }
-}
+int main() {}
--- main-1-2.cpp
+++ main-1-2.cpp
@@ -4,9 +4,4 @@
 #include "ParkingLot.h"
 using namespace std;
 
-int main() {
-    Vehicle* vehicleArray = new Vehicle[3];
-    for (int i = 0; i < 3; i++){
-        vehicleArray[i];
-    }
-}
+int main() {}
