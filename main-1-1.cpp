881933b0b3ff1898c4166b7e8b0ad03e268a92e2
Mohit <a1885648@adelaide.edu.au>
2023-09-05 13:53:36 +0930

    k

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(){
     Appliance a(100);
 
+    cout<< a.getPowerConsumption() <<endl;
     cout<< a.get_powerRating() <<endl;
     cout<< a.get_isOn() <<endl;
-    cout<< a.getPowerConsumption() <<endl;
+    
 }
