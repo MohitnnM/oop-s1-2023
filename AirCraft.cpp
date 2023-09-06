35420e455a948b67c0e310e91eb8626ca6b0ccc1
Mohit <a1885648@adelaide.edu.au>
2023-09-06 13:16:16 +0930

    k

---
+++ AirCraft.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "AirCraft.h"
+using namespace std;
+
+AirCraft::AirCraft(){
+    fuel = 100;
+    numberOfFlights = 0;
+}
+
+AirCraft::AirCraft(int w){
+    this->weight = w;
+}
+
+void AirCraft::set_fuel(float fuel){
+    this->fuel = fuel;
+}
+
+float AirCraft::get_fuel(){
+    return fuel;
+}
+
+void AirCraft::set_weight(int weight){
+    this->weight = weight;
+}
+
+int AirCraft::get_weight(){
+    return weight;
+}
+
+void AirCraft::set_numberOfFlights(int numberofFlights){
+    this->numberOfFlights = numberOfFlights;
+}
+
+int AirCraft::get_numberOfFlights(){
+    return numberOfFlights;
+}
+
+void AirCraft::refuel(){
+    fuel = 100;
+}
+
+void AirCraft::fly(int headwind, int minutes){
+    numberOfFlights += 1;
+}
+
+
