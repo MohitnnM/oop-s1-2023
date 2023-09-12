deb67f1de97ca1d46c1f6ddefcf1faefb525b7d1
Mohit <a1885648@adelaide.edu.au>
2023-09-12 14:24:43 +0930

    k

---
+++ Gradebook.h
@@ -0,0 +1,13 @@
+#ifndef GRADEBOOK_H
+#define GRADEBOOK_H
+#include <iostream>
+
+class Gradebook{
+    private:
+    public:
+        Gradebook();
+        Gradebook();
+        void addGrade(int stud_id, int course_id, string assignment, int value);
+};
+
+#endif
