c4608bc35a58f47bbaeb296aa56ccafd1a62c251
Mohit <a1885648@adelaide.edu.au>
2023-10-17 16:26:45 +1030

    k

---
+++ ws11.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+// vector of int.
+// used to store the records
+typedef vector<int> Records;
+// https://www.cplusplus.com/doc/tutorial/files/
+class RecordsManager {
+ private:
+  fstream _file;
+  string _filename;
+
+ public:
+  RecordsManager(string filename) : _filename(filename) {}
+  void read(Records &records) {
+    _file.open(_filename, ios::in);
+    if (_file.is_open()) {
+      string line;
+      while (std::getline(_file, line)) {
+        records.push_back(stoi(line));
+        try{
+            records.push_back(stoi(line));
+        } catch (const invalid_argument& e){
+            cout << "Invalid Argument" << e.what() << endl;
+            _file.close();
+            return;
+        } catch (const out_of_range& e){
+            cout << "Out Of Range" << e.what() << endl;
+            _file.close();
+            return;
+        }
+      }
+      _file.close();
+    } else {
+        cout<< "Failed to open file" << _filename << endl;
+    }
+  }
+};
+int main() {
+  RecordsManager receordM("test_clean.txt");
+  // RecordsManager receordM("test_corrupt1.txt");
+  // RecordsManager receordM("test_corrupt2.txt");
+  Records myRecords;
+  // reads records
+  receordM.read(myRecords);
+  // calculate and print out the sum
+  int sum = 0;
+  for (int i = 0; i < myRecords.size(); i++) {
+    sum += myRecords[i];
+  }
+  cout << sum << endl;
+  return 0;
+}
