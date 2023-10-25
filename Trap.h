1a3e99dbc398df13d328be8ed232fd5f9a3be19e
Mohit <a1885648@adelaide.edu.au>
2023-10-25 15:06:42 +1030

    k

--- Trap.h
+++ Trap.h
@@ -1,7 +1,21 @@
 #ifndef TRAP_H
 #define TRAP_H
 
-class Trap{
+#include "Cell.h"
+#include "Effect.h"
+
+class Trap : public Cell, public Effect{
+    private:
+        bool active;
+    public:
+        Trap(int x, int y) : Cell(x, y, 'T'){}
+        bool isActive(){
+            return active;
+        }
+        void apply(Cell& cell){
+            cell.setType('T');
+            active = false;
+        }
 
 };
 
