4ef0ecc0fc1b70c2bd5a66d4024e570a86623546
Mohit <a1885648@adelaide.edu.au>
2023-10-25 15:15:08 +1030

    k

--- Character.h
+++ Character.h
@@ -1,8 +1,19 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
-class Character{
+#include "Cell.h"
+class Character: public Cell{
+    public:
+        Character(int x, int y) : Cell(x,y,'C'){}
+        void move(int dx, int dy){
+            int x, y;
+            tuple<int,int> pos = getPos();
+            x = get<0>(pos);
+            y = get<1>(pos);
 
+            setPos(x + dx, y + dy);
+
+        }
 };
 
 #endif
--- main-2.cpp
+++ main-2.cpp
@@ -1,1 +1,10 @@
 #include <iostream>
+#include "Cell.h"
+#include "Trap.h"
+#include "Character.h"
+using namespace std;
+
+int main(){
+    Trap trap(0,0);
+    Character character(0,0);
+}
